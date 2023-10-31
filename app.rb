6f1ec4f5ccd56f4f9190b23cc9fc98053474b4bf
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-10-31 21:03:54 +0000

    Currency Changes

--- app.rb
+++ app.rb
@@ -33,12 +33,13 @@ end
 
 get("/payment/results") do
   rate = (params.fetch("user_apr").to_f)/100
-  @apr = params.fetch("user_apr").to_f.round(4).to_fs(:percentage)
+  @apr = params.fetch("user_apr").to_f.round(5).to_fs(:percentage)
   @user_years = params.fetch("user_years").to_i
-  @user_pv = params.fetch("user_pv").to_f
+  user_pv = params.fetch("user_pv").to_f
+  @user_pv = user_pv.to_fs(:currency)
   n = (@user_years * 12).to_f
   r = rate/12
-  numer = r * @user_pv
+  numer = r * user_pv
   denom = 1 - ((1+r) ** -n) 
 
   m_payment = numer / denom
