e057428775af9d7bdea02d9c3c1204e8eb987c0c
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-10-31 17:01:26 +0000

    Homepage looks complete

--- app.rb
+++ app.rb
@@ -2,10 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
 end
 
 get("/square/new") do
