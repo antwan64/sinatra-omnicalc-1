baed487f4b7c6d827f4ac565a87bcfe02b8fd067
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-10-31 17:16:59 +0000

    Square Root Complete

--- app.rb
+++ app.rb
@@ -16,13 +16,13 @@ get("/square/results") do
   erb(:square_results)
 end
 
-get("/square/new") do
-  erb(:new_square_calc)
+get("/square_root/new") do
+  erb(:new_square_root_calc)
 end
 
-get("/square/results") do
+get("/square_root/results") do
   @the_num = params.fetch("users_number").to_f
-  @the_result = @the_num ** 2
+  @the_result = @the_num ** 0.5
 
-  erb(:square_results)
+  erb(:square_root_results)
 end
